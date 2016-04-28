36c312e814f65ad723db48f6b8cf2edf68d4710f
Tyler Hughes <iampbt@gmail.com>
2016-04-27 23:49:27 -0700

    Add services page content, make footer resilient to blog outages.

--- middleware/getFooterBlogPosts.js
+++ middleware/getFooterBlogPosts.js
@@ -6,10 +6,17 @@ const MAX_POSTS = 5;
 
 module.exports = function(req, res, next) {
   if (req.method !== 'GET') return;
+  res.locals.footerBlogPosts = {
+    error: null,
+    data: []
+  }
   request
     .get(API_URL)
     .end((err, data) => {
-      if (err) return next(err);
+      if (err) {
+        res.locals.footerBlogPosts.error = "There was a problem getting the latest blog posts!"
+        return next();
+      }
       res.locals.posts = _.chain(data.body)
         .take(MAX_POSTS)
         .map(post => {
